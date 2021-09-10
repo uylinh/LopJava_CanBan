df36033700d582bac99990f4b9d8a903a3d62ade
uylinh <nguyenuylinh200302@gmail.com>
2021-09-10 10:20:55 +0700

    hello

---
+++ src/main/java/Bai1/hello.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Bai1;
+
+/**
+ *
+ * @author MY HP
+ */
+public class hello {
+    public static void main(String[] args){
+        System.out.println("Hello");
+   }
+}
